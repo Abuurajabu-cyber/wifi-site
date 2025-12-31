545e64667782b3eecd3a7f20b86450f400c73174
Abuurajabu-cyber <sophiajamess093@gmail.com>
2026-01-01 00:48:23 +0300

    release

---
+++ script.js
@@ -0,0 +1,8 @@
+function scrollToInfo() {
+  document.getElementById("about").scrollIntoView({ behavior: 'smooth' });
+}
+
+function showGreeting() {
+  document.getElementById("welcomeMessage").innerText =
+    "Asante kwa kuona tovuti yetu! Tembelea Kuruthumu Cafe kupata muunganisho wa mtandao wa kasi & huduma bora.";
+}
